b8a5e0c17eef142b82fe5a8b295fa63538362ddc
xuzhenhua <xuzhenhua@baidu.com>
2017-06-30 09:48:35 +0800

    修正左下 Ld 的计算
    之前使用了Lt的计算，导致斜着向下第三个子的时候判断赢（当前1+Lt2+Ld2=5）

--- src/main.js
+++ src/main.js
@@ -45,8 +45,6 @@ oStage.addComponent(oGameCancelUndoText);
 Gomoku.inGame = false;
 Gomoku.board = {};
 
-
-// [x,y,color]
 /**
  * 下在某处
  */
@@ -229,8 +227,9 @@ Gomoku.calcIsWin = (arrPos, color) => {
     }
     //   左下 , [ x++  , y++]
     for (let i = 1; i < 5; i++) {
-        let x = arrPos[0] - i;
-        let y = arrPos[1] - i;
+
+        let x = arrPos[0] - 0 + i;
+        let y = arrPos[1] - 0 + i;
 
         if (x < 0 || y < 0) {
             break;
@@ -253,16 +252,11 @@ Gomoku.calcIsWin = (arrPos, color) => {
 
 
     return [
-        lib.prevXSum + lib.nextXSum,    // x
-        lib.prevYSum + lib.nextYSum,     // y
+        lib.prevXSum + lib.nextXSum,      //   -
+        lib.prevYSum + lib.nextYSum,      //    |
         lib.prevRtSum + lib.prevRdSum,    //   /
-        lib.prevLtSum + lib.prevLdSum    //   \
+        lib.prevLtSum + lib.prevLdSum     //   \
     ];
-    //
-    // let min = Math.min(x - 4, 0);
-    // let max = Math.min(x + 4, 14);
-
-    // let list = Gomoku.board
 };
 
 /**
@@ -283,3 +277,5 @@ Gomoku.restart = (isStart) => {
 
 };
 Gomoku.restart(false);
+
+[["2_4","black"],["2_5","white"],["2_6","black"],["2_7","white"],["2_8","black"],["2_9","white"],["2_10","black"],["2_11","white"],["3_11","black"],["4_11","white"],["5_11","black"],["6_11","white"],["7_11","black"],["8_11","white"],["9_11","black"],["10_11","white"],["11_11","black"],["12_11","white"],["12_10","black"],["12_9","white"],["12_8","black"],["12_7","white"],["12_6","black"],["12_5","white"],["12_4","black"],["6_10","white"],["6_9","black"],["6_8","white"],["6_7","black"],["7_7","white"],["8_7","black"],["8_8","white"],["8_9","black"],["8_10","white"],["7_10","black"],["7_9","white"],["7_8","black"],["1_4","white"],["1_5","black"],["1_6","white"],["1_8","black"],["1_7","white"],["1_9","black"],["1_10","white"],["1_11","black"],["13_11","white"],["13_10","black"],["13_9","white"],["13_8","black"],["13_7","white"],["13_6","black"],["13_5","white"],["13_4","black"]].forEach((i)=>{Gomoku.setIn(i[0]);})
